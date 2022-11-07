9e1d52b3f17063f9d17028be558fd1670346c67e
CasaverdeDiego <111712253+CasaverdeDiego@users.noreply.github.com>
2022-11-07 00:20:19 -0500

    Add files via upload

---
+++ backend/src/main/java/pe/com/bikerent/backend/services/DeliveryService.java
@@ -0,0 +1,17 @@
+package pe.com.bikerent.backend.services;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import pe.com.bikerent.backend.entities.Delivery;
+
+import java.util.List;
+
+public interface DeliveryService {
+    public Delivery createDeliveryS(Delivery delivery);
+    public Delivery updateDeliveryByIdS(Delivery foundDelivery);
+    public List<Delivery> getAllDeliveriesS();
+    public Delivery getDeliveryByIdS(Long id);
+    public List<Delivery> getALLDeliveriesByRepartidorS(String repartidor);
+}
