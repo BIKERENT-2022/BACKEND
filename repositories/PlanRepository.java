8e9f6503b129139c7a7b22a7d73684f2bd2b81e9
CasaverdeDiego <111712253+CasaverdeDiego@users.noreply.github.com>
2022-11-07 01:54:40 -0500

    Delete repositories directory

--- repositories/PlanRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.com.bikerent.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.com.bikerent.backend.entities.Plan;
-
-public interface PlanRepository extends JpaRepository<Plan,Long> {
-}
